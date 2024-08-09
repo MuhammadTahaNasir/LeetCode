5f33bae826c23aff369d7f27d20acae63dbc7bda
Muhammad Taha Nasir <146159808+MuhammadTahaNasir@users.noreply.github.com>
2024-08-09 23:46:11 +0500

    Time: 38 ms (69.93%), Space: 36.2 MB (57.68%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.cpp
@@ -2,19 +2,14 @@ class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
         vector<int> result;
-        
-        for (int i = 0; i < nums.size(); ++i) {
-            int index = abs(nums[i]) - 1;
-            
-            // If the number at this index is negative, it means the number has been seen before
+        for (int num : nums) {
+            int index = abs(num) - 1;
             if (nums[index] < 0) {
                 result.push_back(index + 1);
             } else {
-                // Mark the number as seen by negating the value at that index
                 nums[index] = -nums[index];
             }
         }
-        
         return result;
     }
 };
