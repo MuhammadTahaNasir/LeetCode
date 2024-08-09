123734a870ae8df2038990a3fe03d682087b6739
Muhammad Taha Nasir <146159808+MuhammadTahaNasir@users.noreply.github.com>
2024-08-10 03:55:54 +0500

    Time: 7 ms (70.64%), Space: 21.4 MB (37.04%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
-    int removeDuplicates(std::vector<int>& nums) {
-        if (nums.empty()) return 0; // Handle the edge case of an empty array
-        
-        int i = 0; // `i` is the slow pointer
-
-        for (int j = 1; j < nums.size(); j++) { // `j` is the fast pointer
-            if (nums[j] != nums[i]) { // Found a unique element
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0, j = 1;
+        while (j < n) {
+            if (nums[i] != nums[j]) { // Here We get unique number
                 i++;
-                nums[i] = nums[j]; // Move the unique element forward
+                nums[i]=nums[j];
             }
+            j++;
         }
-        
-        return i + 1; // Length of the array with unique elements
+        return i + 1;
     }
 };
