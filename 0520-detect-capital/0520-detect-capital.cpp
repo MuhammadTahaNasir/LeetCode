92a6a4a7ce9a81ea2960f775c129221a93dd4883
Muhammad Taha Nasir <m.tahanasir.cs@gmail.com>
2025-03-20 15:00:00 +0500

    Time: 6 ms (98.4%), Space: 14.3 MB (92.1%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int capitals = 0;
+        for (char c : word) {
+            if (isupper(c)) capitals++;
+        }
+        return capitals == 0 || capitals == word.length() || (capitals == 1 && isupper(word[0]));
+    }
+};
