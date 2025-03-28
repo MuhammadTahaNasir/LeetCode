729b505c4543e90f37a0c64f36f3195c3b725df0
Muhammad Taha Nasir <m.tahanasir.cs@gmail.com>
2025-03-28 15:00:00 +0500

    Time: 7 ms (80.5%), Space: 15.8 MB (75.8%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if (head == nullptr || head->next == nullptr) {
+            return true; // Empty list or single node list is a palindrome
+        }
+
+        // Step 1: Find the middle of the list
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Step 2: Reverse the second half of the list
+        ListNode* prev = nullptr;
+        ListNode* curr = slow;
+
+        while (curr != nullptr) {
+            ListNode* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        // Step 3: Compare the first and the second half
+        ListNode* head1 = head;
+        ListNode* head2 = prev;  // prev is now the head of the reversed second half
+
+        while (head2 != nullptr) {
+            if (head1->val != head2->val) {
+                return false;
+            }
+            head1 = head1->next;
+            head2 = head2->next;
+        }
+
+        return true;  // All matched, it is a palindrome
+    }
+};
