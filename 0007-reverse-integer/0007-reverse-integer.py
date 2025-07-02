c5259540fdcad3e0d80cb6630475d67ce7f0baac
Muhammad Taha Nasir <146159808+MuhammadTahaNasir@users.noreply.github.com>
2025-07-02 20:20:28 +0500

    Time: 53 ms (7.18%), Space: 17.9 MB (36.7%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -4,13 +4,14 @@ class Solution:
         INT_MIN = -2**31
 
         rev = 0
-        sign = -1 if x < 0 else 1
+        sign = 1 if x >= 0 else -1
         x = abs(x)
 
         while x != 0:
             digit = x % 10
             x //= 10
 
+            # Check for overflow before updating rev
             if rev > (INT_MAX - digit) // 10:
                 return 0
 
