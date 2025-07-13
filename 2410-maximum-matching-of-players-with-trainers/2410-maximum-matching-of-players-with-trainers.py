7aef431d5a759ed51ef4549e32491ba262c678ca
Muhammad Taha Nasir <146159808+MuhammadTahaNasir@users.noreply.github.com>
2025-07-13 23:17:58 +0500

    Time: 75 ms (71.43%), Space: 34.1 MB (10.66%) - LeetHub

--- 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.py
+++ 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.py
@@ -2,15 +2,14 @@ class Solution:
     def matchPlayersAndTrainers(self, players, trainers):
         players.sort()
         trainers.sort()
-        players = deque(players)
-        trainers = deque(trainers)
-        matches = 0
-
-        while players and trainers:
-            if players[0] <= trainers[0]:
-                players.popleft()
-                trainers.popleft()
+        
+        i = j = matches = 0
+        len_p, len_t = len(players), len(trainers)
+        
+        while i < len_p and j < len_t:
+            if players[i] <= trainers[j]:
                 matches += 1
-            else:
-                trainers.popleft()  # Trainer too weak
+                i += 1
+            j += 1
+            
         return matches
