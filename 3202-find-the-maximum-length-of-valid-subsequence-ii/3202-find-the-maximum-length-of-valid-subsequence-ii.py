8015dbc2ca0c08772e7eb6cecf6db67849b2a209
Muhammad Taha Nasir <146159808+MuhammadTahaNasir@users.noreply.github.com>
2025-07-17 05:29:12 +0500

    Time: 1980 ms (55.74%), Space: 25.7 MB (35.25%) - LeetHub

--- 3202-find-the-maximum-length-of-valid-subsequence-ii/3202-find-the-maximum-length-of-valid-subsequence-ii.py
+++ 3202-find-the-maximum-length-of-valid-subsequence-ii/3202-find-the-maximum-length-of-valid-subsequence-ii.py
@@ -1,14 +1,15 @@
 class Solution:
     def maximumLength(self, nums: List[int], k: int) -> int:
-        # dp[i][j] = length of longest valid subsequence
-        # ending with last % k == i and expecting next such that (prev + next) % k == j
+        # dp[x][y] = length of longest valid subsequence where:
+        # last element % k == x, second-to-last % k == y
         dp = [[0] * k for _ in range(k)]
         ans = 0
 
-        for x in nums:
-            r = x % k
+        for num in nums:
+            x = num % k
             for j in range(k):
-                dp[r][j] = dp[j][r] + 1
-                ans = max(ans, dp[r][j])
+                y = (j - x + k) % k
+                dp[x][y] = dp[y][x] + 1
+                ans = max(ans, dp[x][y])
 
         return ans
